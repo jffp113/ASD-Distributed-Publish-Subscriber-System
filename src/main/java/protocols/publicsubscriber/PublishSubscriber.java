62937d2db0fcef270c9db26f85aa4f26cb84a4ec
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2019-09-22 19:42:21 +0100

    Correct Buggy name

--- src/main/java/protocols/publicsubscriber/PublishSubscriber.java
+++ src/main/java/protocols/publicsubscriber/PublishSubscriber.java
@@ -6,13 +6,13 @@ import network.INetwork;
 import java.util.Properties;
 
 //TODO implement this protocol with the correspondent methods
-public class PublicSubscriber extends GenericProtocol{
+public class PublishSubscriber extends GenericProtocol{
 
     public final static short PROTOCOL_ID = 100;
-    public final static String PROTOCOL_NAME = "Public/Subscriber";
+    public final static String PROTOCOL_NAME = "Publish/Subscriber";
 
 
-    public PublicSubscriber(INetwork net) {
+    public PublishSubscriber(INetwork net) {
         super(PROTOCOL_NAME, PROTOCOL_ID, net);
     }
 
