2b618e7cda782cf1d05c92edd0cd51cd3fabf429
cBartolomeu <43549@alunos.isel.ipl.pt>
2019-09-25 19:03:46 +0100

    InteractiveClient Refactor

--- src/main/java/client/InteractiveClient.java
+++ src/main/java/client/InteractiveClient.java
@@ -10,32 +10,40 @@ public class InteractiveClient {
     public static final String UNSUBSCRIBE = "unsubscribe";
     public static final String PUBLISH = "publish";
     public static final String HELP = "help";
+    public static final String HELP_TEXT = "subscribe <topic> - subscribe to a topic.\n" +
+            "unsubscribe <topic> - unsubscribe to a topic.\n" +
+            "publish <topic> <message> - publish a message in the given topic.\n" +
+            "exit - shutdown the client.";
+    public static final String SUBSCRIBE_TEXT = "Subscribing: %s\n";
+    public static final String UNSUBSCRIBE_TEXT = "Unsubscribing: %s\n";
     public static String EMPTY_STRING = "";
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Client c = new Client(args);
         Scanner in = new Scanner(System.in);
         String input = EMPTY_STRING;
         while (!input.equalsIgnoreCase(EXIT)) {
             input = in.nextLine();
             String[] input_array = input.split(SPACE);
-            switch(input_array[0]){
+            String command = input_array[0],
+                    topic = input_array.length == 2 ? input_array[1] : EMPTY_STRING,
+                    message = input_array.length == 3 ? input_array[2] : EMPTY_STRING;
+            switch (command) {
                 case SUBSCRIBE:
-                    System.out.println("Subscribing: " + input_array[1]);
-                    c.subscribe(input_array[1]);
+                    System.out.printf(SUBSCRIBE_TEXT, topic);
+                    c.subscribe(topic);
                     break;
                 case UNSUBSCRIBE:
-                    System.out.println("Unsubscribing: " + input_array[1]);
-                    c.unsubscribe(input_array[1]);
+                    System.out.printf(UNSUBSCRIBE_TEXT, topic);
+                    c.unsubscribe(topic);
                     break;
                 case PUBLISH:
-                    c.publish(input_array[1],input_array[2]);
+                    c.publish(topic, message);
                     break;
                 case HELP:
-                    System.out.println("HELP SENPAI!");
+                    System.out.println(HELP_TEXT);
                     break;
             }
-
         }
     }
 }
